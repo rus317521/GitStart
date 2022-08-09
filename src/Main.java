68128b62b9c4b846ca9f9d53e2858105243282a1
rus317521 <rus317521@yandex.ru>
2022-08-09 21:06:56 +0500

    Formated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int sales[] = {20, 30, 100, 50, 30};
+
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println("Максимальная продажа: " + salesManager.max());
+
+
+    }
 }
+
